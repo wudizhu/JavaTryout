054698de372dcbae86bc90870694713bf7dc5052
wudizhu <zhang_hy@hotmail.com>
2020-05-09 21:52:49 +0100

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 10;
+        var b = a;
+        System.out.println("Hello World!");
+        System.out.println(b);
+    }
+}
